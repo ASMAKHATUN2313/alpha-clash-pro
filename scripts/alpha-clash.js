a6b90f04800575b0475d1e43deee64679cc7326a
asmakhatun2313 <asmakhatun2313.gmail.com>
2024-02-16 10:27:42 -0300

    alpha clash-1 complete

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,9 +1,35 @@
-function play(){
-    //hide the home screen. to hide the screen add the class hidden to the home section.
-const homeSection= document. getElementById('home-screen');
-homeSection.classList.add('hidden');
-    //show playground
+// function play(){
+//     //hide the home screen. to hide the screen add the class hidden to the home section.
+// const homeSection= document. getElementById('home-screen');
+// homeSection.classList.add('hidden');
+//     //show playground
 
-    const playGroundSection=document.getElementById('play-ground');
-    playGroundSection.classList.remove('hidden')
+//     const playGroundSection=document.getElementById('play-ground');
+//     playGroundSection.classList.remove('hidden')
+// }
+
+// function continueGame(){
+//     //step-1: generate a random table 
+
+// }
+function continueGame(){
+   //step-1: generate a random alphabet
+
+   const alphabet= getARandomAlphabet();
+   console.log('your random alphabet',alphabet);
+
+// set randomly generated alphabet to the screen(show it)
+
+const currentAlphabetElement= document.getElementById('current-alphabet');
+currentAlphabetElement.innerText=alphabet;
+
+ //set background color
+setBackgroundColorById(alphabet);
+
+}
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame('');
 }
